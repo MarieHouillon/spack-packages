c2bf8ace56adf057f3f71d9487f110b1f62b4705
Grech Kitio <grech.kitio@kit.edu>
2026-03-24 07:16:44 +0100

    spack: update ginkgo package for openCARP compatibility

--- repos/spack_repo/builtin/packages/ginkgo/package.py
+++ repos/spack_repo/builtin/packages/ginkgo/package.py
@@ -30,9 +30,9 @@ class Ginkgo(CMakePackage, CudaPackage, ROCmPackage):
     version("develop", branch="develop")
     version("main", branch="main")
     version("master", branch="master", deprecated=True)
-    version("1.11.0", commit="4c77a5d826bd1597fd093f39c47688f8cc735690")  # v1.11.0
-    version("1.10.0", commit="d4e0e9f8c8eb36cc2044189834d82742b925f27e")  # v1.10.0
-    version("1.9.0", commit="20cfd68795f58078898da9890baa311b46845a8b")  # v1.9.0
+    version("1.11.0", commit="4c77a5d826bd1597fd093f39c47688f8cc735690", submodules=True)  # v1.11.0
+    version("1.10.0", commit="d4e0e9f8c8eb36cc2044189834d82742b925f27e", submodules=True)  # v1.10.0
+    version("1.9.0", commit="20cfd68795f58078898da9890baa311b46845a8b", submodules=True)  # v1.9.0
     version("1.8.0", commit="586b1754058d7a32d4bd1b650f9603484c2a8927")  # v1.8.0
     version("1.7.0", commit="49242ff89af1e695d7794f6d50ed9933024b66fe")  # v1.7.0
     version("1.6.0", commit="1f1ed46e724334626f016f105213c047e16bc1ae")  # v1.6.0
@@ -125,7 +125,7 @@ class Ginkgo(CMakePackage, CudaPackage, ROCmPackage):
     conflicts("^cuda@12.4", when="+cuda", msg="CCCL 2.3 bug causes build failure.")
 
     # https://github.com/ginkgo-project/ginkgo/pull/1926
-    conflicts("^cuda@13:", when="@:1.10.0 +cuda")
+    #conflicts("^cuda@13:", when="@:1.10.0 +cuda")
 
     # error due to change in warpSize constant definition in ROCm 7.0 prior to v.1.11.0
     # https://github.com/ginkgo-project/ginkgo/pull/1954
@@ -145,7 +145,7 @@ class Ginkgo(CMakePackage, CudaPackage, ROCmPackage):
 
     # Revert the fix from github.com/ginkgo-project/ginkgo/pull/1954/changes
     # This only affects the benchmark part of Ginkgo, which is not built by spack anyway
-    patch("remove_finding_thrust.patch", when="@1.11.0 +cuda")
+    patch("remove_finding_thrust.patch", when="@1.9.0 +cuda")
 
     # Correctly find rocthrust through CMake
     patch(
